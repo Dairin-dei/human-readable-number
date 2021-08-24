1487d59161b9beb892de9704f3f3d2aea450e7bf
Dairin <deirin@yandex.ru>
2021-08-24 14:50:17 +0300

    init: change human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
-  
+    let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];   
+    let tensTitles = ['','','twenty','thirty','forty', 'fifty', 'sixty','seventy','eighty','ninety'];
+
+   let numberUnits = number%10;
+    let convertUnits = units[numberUnits];
+
+    if (number === 0){
+      return 'zero';
+    };
+
+    if (number<=9){
+      return convertUnits};
+
+    let numberTens = number%100;
+ 
+    let convertTens = '';
+
+    if (numberTens>=10&&numberTens<=19){
+      convertTens = ' '+teens[numberTens-10];
+      convertUnits = ''
+    }
+    else {
+      numberTens = Math.floor(numberTens/10);
+      convertTens = numberTens!==0 ? ' '+tensTitles[numberTens]:'';
+    }
+
+    if (number<=99){
+      return (convertTens+' '+convertUnits).trim()};
+
+    numberHundreds = Math.floor(number/100);
+    let convertHundreds = '';
+
+    convertHundreds = units[numberHundreds];
+
+
+    return (convertHundreds+' hundred'+convertTens+' '+convertUnits).trim();
 }
